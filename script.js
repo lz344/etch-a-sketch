eb6998db67927f918f0bed1e4f1c613b33bf425e
lz344 <luiz.adriano.lz.l@gmail.com>
2025-01-13 23:11:33 -0300

    Adiciona os estágios iniciais do js e css
    No JS: foi criado o fluxo de criação dos quadrados (.square) dentro da div
    (#container-ea). Tudo já está sendo feito dinamicamente.
    
    No CSS: no seletor (#container-ea) foi feita a configuração do display:flex.
    Foi definida também uma variável global no CSS para a mudança das dimensões dos quadrados
    que serão criados dinamicamente no JS.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+let userInput = +prompt("Quantos quadrados? ");
 
+let grid = userInput * userInput;
+let squareSize = 960 / userInput;
+
+for (let i = 0; i < grid; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+  document.getElementById("container-eas").appendChild(square);
+}
+
+document.documentElement.style.setProperty("--square-size", `${squareSize}px`);
