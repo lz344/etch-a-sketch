39a602b119f1b54d2ea8e1caec74dbc5502b7ac0
lz344 <luiz.adriano.lz.l@gmail.com>
2025-01-14 22:35:35 -0300

    Cria um campo para o usuario entrar com o dado
    Foi criado um botão também, e juntamente com ele, um eventListener.
    Esse botão, quando clicado, ativa a função createSquares(),
    que é agora a responsável por fazer os cálculos dos quadrados.

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-let userInput = +prompt("Quantos quadrados? ");
+const textFieldUser = document.querySelector("#how-many-squares");
+const submitButton = document.querySelector("#button-calc-squares");
 
-let grid = userInput * userInput;
-let squareSize = 960 / userInput;
+function createSquares(input) {
+  let userInput = input.value;
+  let grid = userInput * userInput;
+  let squareSize = 960 / userInput;
+  document.documentElement.style.setProperty(
+    "--square-size",
+    `${squareSize}px`
+  );
 
-for (let i = 0; i < grid; i++) {
-  const square = document.createElement("div");
-  square.classList.add("square");
-  document.getElementById("container-eas").appendChild(square);
+  for (let i = 0; i < grid; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    document.querySelector("#container-eas").appendChild(square);
+  }
+
+  input.value = "";
 }
 
-document.documentElement.style.setProperty("--square-size", `${squareSize}px`);
+submitButton.addEventListener("click", () => {
+  createSquares(textFieldUser);
+});
