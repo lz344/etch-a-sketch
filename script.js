a82218a0f6b88e36b492fc88f212329800d12fe0
lz344 <luiz.adriano.lz.l@gmail.com>
2025-01-17 22:25:39 -0300

    Adiciona mais lógica ao evento da função createSquares()
    O objetivo da lógica contruída é deixar cada quadrado com uma cor única e aleatória
    , além de, fazer com que as cores deles fiquem mais vibrantes na medida que o usuário
    passar com o ponteiro do mouse em cima do mesmo.

--- script.js
+++ script.js
@@ -13,9 +13,44 @@ function createSquares(input) {
   for (let i = 0; i < grid; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
-    square.addEventListener("mouseenter", () => {
-      console.log("passou por aqui!");
+
+    square.addEventListener("mouseenter", function () {
+      const colorHex = [
+        "0",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+      ];
+      if (this.style.backgroundColor === "") {
+        let backgroundColor = "";
+        for (let i = 0; i < 6; i++) {
+          let aleatory = Math.floor(Math.random() * 16);
+          backgroundColor += colorHex[aleatory];
+        }
+        this.style.backgroundColor = `#${backgroundColor}`;
+      } else {
+        if (this.style.opacity === "") {
+          this.style.opacity = 0.2;
+        } else {
+          if (+this.style.opacity <= 1.1) {
+            this.style.opacity = `${+this.style.opacity + 0.1}`;
+          }
+        }
+      }
     });
+
     document.querySelector("#container-eas").appendChild(square);
   }
 
